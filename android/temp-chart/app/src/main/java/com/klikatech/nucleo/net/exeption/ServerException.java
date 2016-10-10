4835cf0cf60b0ea7024db1d85c79b7cffb8529ac
Eugene Volchek <evolchek@klika-tech.com>
2016-10-10 15:35:34 -0400

    legacy app removed

--- android/temp-chart/app/src/main/java/com/klikatech/nucleo/net/exeption/ServerException.java
+++
@@ -1,9 +0,0 @@
-package com.klikatech.nucleo.net.exeption;
-
-
-public class ServerException extends Throwable {
-
-    public ServerException(String detailMessage, Throwable cause) {
-        super(detailMessage, cause);
-    }
-}
